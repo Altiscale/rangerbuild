97d09c547571e80bdfa419094e06f9542ec59dd5
Ravi Prakash <raviprak@altiscale.com>
2017-10-23 15:40:29 -0700

    Hopefully build should work now

--- scripts/justbuild.sh
+++ scripts/justbuild.sh
@@ -1,18 +1,10 @@
 #!/bin/sh -ex
 # build the artifacts using the "pure" maven builds
 
-cd ${WORKSPACE}/hadoop-lzo
-mvn versions:set -DnewVersion=0.4.18-${DATE_STRING}
-if [ "$RUN_UNIT_TESTS" == "true" ]; then
-  mvn clean package
-else
-  mvn clean package -DskipTests
-fi
-
-cd ${WORKSPACE}/hadoop
+cd ${WORKSPACE}/ranger
 mvn versions:set -DnewVersion=${ARTIFACT_VERSION}
 if [ "$RUN_UNIT_TESTS" == "true" ]; then
-  mvn -Pdist,docs,src,native --fail-never -Dtar -Dbundle.snappy  -Dsnappy.lib=/usr/lib64 -Drequire.fuse=true -Drequire.snappy -Dcontainer-executor.conf.dir=/etc/hadoop clean install
+  mvn clean install assembly:assembly
 else
-  mvn -Pdist,docs,src,native -Dtar -DskipTests -Dbundle.snappy -Dsnappy.lib=/usr/lib64 -Drequire.fuse=true -Drequire.snappy -Dcontainer-executor.conf.dir=/etc/hadoop clean install
+  mvn -DskipTests clean install assembly:assembly
 fi
