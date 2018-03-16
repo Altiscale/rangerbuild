f3dc66db566b5b5012e409be462e7234e8628589
Sahin <cetin@altiscale.com>
2018-03-16 16:46:17 -0700

    Adding SourceClear scan support for Ranger builds

--- scripts/justbuild.sh
+++ scripts/justbuild.sh
@@ -2,9 +2,20 @@
 # build the artifacts using the "pure" maven builds
 
 cd ${WORKSPACE}/ranger
+
+if [ "$RUN_SOURCECLEAR_SCAN" == "true" ]; then
+  curl -sSL https://download.sourceclear.com/ci.sh | DEBUG=1 sh -s -- scan  --no-upload
+fi
+
+echo "Before mvn command - Directory Structure"
+ls -l
 mvn versions:set -DnewVersion=${ARTIFACT_VERSION}
 if [ "$RUN_UNIT_TESTS" == "true" ]; then
-  mvn clean install assembly:assembly
+  mvn ${JUSTBUILD_EXTRA_OPTS} clean install assembly:assembly 
 else
-  mvn -DskipTests clean install assembly:assembly
+  mvn ${JUSTBUILD_EXTRA_OPTS} -DskipTests clean install assembly:assembly 
 fi
+
+ls -l
+echo "After mvn command - Directory Structure"
+ls -l
